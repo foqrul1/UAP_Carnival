3bd01066cd12358b66b2620d1ffea6b3d53381e7
Foqrul <foqrulislam426@gmail.com>
2018-05-13 10:27:37 +0600

    initial

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
      
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Rohingya Issue</title>
+        <title>Uap Carnival</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -19,7 +19,7 @@
           <header>
             <div class="row" style="padding-top: 20px;">
               <div class="col-5">
-                <h1 class="h1" id="logo" style="color: #f3bf9f; font-family: 'Permanent Marker', cursive;">Rohingya Issue</h1>
+                <h1 class="h1" id="logo" style="color: #f3bf9f; font-family: 'Permanent Marker', cursive;">Uap Carnival</h1>
               </div>
               <div class="col-6">
                 <ul class="nav nav-tabs" style=" border-bottom: 0px; float: right;">
